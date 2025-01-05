2e23dcee143221b47fd7df35b22c0d1f78f79844
Patrick <patrick_btc_96@hotmail.com>
2025-01-05 13:11:19 +0100

    added canvas bg

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,9 +1,19 @@
+import Squares from "./squares-bg/Squares";
 import { Navigation } from "./Navigation";
 import { Outlet } from "react-router";
 
 export const Root = () => {
   return (
     <div>
+      <div className="squares-bg">
+        <Squares
+          speed={0.5}
+          squareSize={40}
+          direction="down"
+          borderColor="#008000"
+          hoverFillColor="rgba(34,34,34,0.7)"
+        />
+      </div>
       <Navigation />
       <Outlet />
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Root } from "./components/Root.tsx";
 import { Home } from "./pages/Home.tsx";
